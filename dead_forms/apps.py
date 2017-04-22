d4ed34f5390fa502a34d2e3c67d2004216df374d
000PaRaDoX000 <ariel.calzada@gmail.com>
2017-04-22 18:55:01 -0500

    Initial dead forms 2

--- dead_forms/apps.py
+++ dead_forms/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class DarFormsConfig(AppConfig):
-    name = 'dar_forms'
+class DEADFormsConfig(AppConfig):
+    name = 'dead_forms'
